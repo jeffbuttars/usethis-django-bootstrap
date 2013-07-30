d65610a810dd365d6fc39ce889336f12a013d437
Jeff Buttars <jeffbuttars@gmail.com>
2013-07-30 12:58:58 -0600

    Update setup.py description.

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ from distutils.core import setup
 
 setup(name='usethis-django-bootstrap',
       version='1.0.0-2.3.2',
-      description="Bootstrap for Django with themes",
+      description="Bootstrap for Django with themes and theme chooser.",
       author="Jeff Buttars",
       author_email="jeffbuttars@gmail.com",
       packages=['usethis_bootstrap'],
       url='https://github.com/jeffbuttars/usethis-django-bootstrap',
-      # data_files=[('static/bootstrap/img', 'static/bootstrap/img/*.png')],
       package_dir={'usethis_bootstrap': 'usethis_bootstrap'},
       package_data={'usethis_bootstrap': 
                     ['static/css/bsthemes/*/*.css',
