09cd49e6b00d9bcc3c8075b0a1f9f92d40f8cc7c
Jeff Buttars <jeffbuttars@gmail.com>
2013-07-30 11:16:10 -0600

    Add manifest.in

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='usethis-django-bootstrap',
+      version='1.0.0-2.3.2',
+      description="Bootstrap for Django with themes",
+      author="Jeff Buttars",
+      author_email="jeffbuttars@gmail.com",
+      packages=['usethis_bootstrap'],
+)
