ec92f9d132c3ea89858c28c3ee31f9ef25123f59
Jeff Buttars <jeffbuttars@gmail.com>
2014-02-16 15:01:11 -0700

    Tweaks and upgrades for bs 3.1

--- devproject/udb/udb/settings.py
+++ devproject/udb/udb/settings.py
@@ -145,7 +145,7 @@ if DEBUG:
 ADMIN_MEDIA_PREFIX = '%sadmin/' % (STATIC_URL)
 
 # Additional locations of static files
-# STATICFILES_DIRS = (
+ # STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
