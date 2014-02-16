e3d545e57ff33c6fc1aac8be41a7a23cc3176424
Jeff Buttars <jeffbuttars@gmail.com>
2014-02-16 13:06:07 -0700

    bump version in setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='usethis-django-bootstrap',
+      version='3.1.0',
+      description="Bootstrap for Django with themes and theme chooser.",
+      author="Jeff Buttars",
+      author_email="jeffbuttars@gmail.com",
+      packages=['usethis_bootstrap'],
+      url='https://github.com/jeffbuttars/usethis-django-bootstrap',
+      license='MIT',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Browsers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+      ],
+      package_dir={'usethis_bootstrap': 'usethis_bootstrap'},
+      package_data={'usethis_bootstrap': 
+                    ['static/bootstrap/css/*.css',
+                     'static/bootstrap/img/*.png',
+                     'static/bootstrap/js/*.js',
+                     'static/bootstrap/*_css/*.css',
+                     'templates/*.html',
+                    ]}
+)
