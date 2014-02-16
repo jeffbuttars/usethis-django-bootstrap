a81b0775358a9a7a4c51f23356ed5bc4b7974a7c
Jeff Buttars <jeffbuttars@gmail.com>
2014-02-16 14:34:51 -0700

    Add the debug css files for bootswatch themes.

--- src/usethis_bootstrap/getbswatch.py
+++ src/usethis_bootstrap/getbswatch.py
@@ -28,24 +28,23 @@ def main():
     ver = data['version']
     themes = data['themes']
     for theme in themes:
-        #print("%s : %s" % (theme['name'], theme['description']))
-        # print("{name} {cssMin}".format(**theme))
-        print("Downloading {name} {cssMin}".format(**theme))
-        resp = requests.get(theme['cssMin'])
-        if resp.status_code == 200:
-            print("Downloaded {name}".format(**theme))
-            theme_dir = os.path.join(
-                outdir,
-                "{}_css".format(theme['name'].lower()))
-            if os.path.exists(theme_dir):
-                shutil.rmtree(theme_dir)
-            os.makedirs(theme_dir)
-            outfile = os.path.join(theme_dir, 'bootstrap.min.css')
-            fd = open(outfile, 'w')
-            # print(dir(resp))
-            fd.write(resp.content)
-            print("Wrote {}\n".format(outfile))
-
+        for ct in ('css', 'cssMin'):
+            print("Downloading {} {}".format(theme['name'], theme[ct]))
+            resp = requests.get(theme[ct])
+            if resp.status_code == 200:
+                print("Downloaded {name}".format(**theme))
+                theme_dir = os.path.join(
+                    outdir,
+                    "{}_css".format(theme['name'].lower()))
+                if not os.path.exists(theme_dir):
+                    os.makedirs(theme_dir)
+                outfile = os.path.join(theme_dir, os.path.basename(theme[ct]))
+                fd = open(outfile, 'w')
+                # print(dir(resp))
+                fd.write(resp.content)
+                print("Wrote {}\n".format(outfile))
+                fd.close()
+        # end for ct in ('css', 'cssMin')
     # end for theme in themes
 # main()
 
